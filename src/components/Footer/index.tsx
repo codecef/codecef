fffb1ac44b26b0d1bcf5b7075f47923f17053a00
ahmd <ahmad.raja@stackfusion.io>
2025-01-26 23:25:00 +0530

    final footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -27,8 +27,7 @@ const Footer = () => {
                   />
                 </Link>
                 <p className="mb-9 text-base leading-relaxed text-body-color dark:text-body-color-dark">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Integer lobortis.At CodeCEF, we deliver innovative, scalable software solutions designed to drive business growth
+                At CodeCEF, we deliver innovative, scalable software solutions designed to drive business growth
                 </p>
                 <div className="flex items-center">
                   <a
