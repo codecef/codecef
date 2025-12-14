48f2eb8c8447fa0c4b1361f63cd7f979937d043c
the@coder <ahmadraza001912@gmail.com>
2025-12-14 20:48:55 +0530

    put only my own number

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -33,7 +33,7 @@ const Hero = () => {
                     href=""
                     className="inline-block rounded-sm bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
                   >
-                    7470668602,9352494160
+                    7470668602
                   </Link>
                 </div>
               </div>
