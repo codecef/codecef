dfdc74184766a037deaab6b7cbfebbc14fa4ab22
the@coder <ahmadraza001912@gmail.com>
2025-12-17 17:42:13 +0530

    remove the duplicate pricing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,6 @@ export default function Home() {
       <AboutSectionOne />
       <AboutSectionTwo />
       {/* <Testimonials /> */}
-      <Pricing />
       {/* <Blog /> */}
       <Contact />
     </>
